4aa180b83d945c956825c851a16170646d6d39a0
nasim-007 <nasim.mahmud.1996@gmail.com>
2022-12-10 15:53:46 +0600

    updates

--- nimusoft/settings.py
+++ nimusoft/settings.py
@@ -117,6 +117,6 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-# Default primary key field type
+
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
