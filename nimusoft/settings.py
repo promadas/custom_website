4c6d4aa69d376a75145ca7b52fe3b21c4117f94a
nasim-007 <nasim.mahmud.1996@gmail.com>
2022-12-26 15:39:51 +0600

    updates site

--- nimusoft/settings.py
+++ nimusoft/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'nimusoft.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-'''  
+''' 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
